7d368745ace690c770ebcc4666944027ee40012a
Daniel Silva Marcelino <systems_analyst@HACKER.Programmer>
2017-08-18 20:28:46 -0300

    Atividade

---
+++ HelloWorld_Python3.py
@@ -0,0 +1,16 @@
+'''
+Python 3
+
+Python é uma linguagem de programação de alto nível, interpretada, de script,
+imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.
+
+Python suporta a maioria das técnicas da programação orientada a objeto.
+Qualquer objeto pode ser usado para qualquer tipo, e o código funcionará
+enquanto haja métodos e atributos adequados. O conceito de objeto na linguagem
+é bastante abrangente: classes, funções, números e módulos são todos considerados
+objetos. Também há suporte para metaclasses, polimorfismo, e herança
+(inclusive herança múltipla). Há um suporte limitado para variáveis privadas.
+'''
+
+# Aqui um "Alá mundo" na linguagem Python
+print ('Hello World')
